7b4dde1651abf668e3120e57a4c6a7ac24ee6562
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-01 03:31:52 -0800

    height to be revisited

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -40,16 +40,3 @@ size_t calculate_height(const binary_tree_t *tree, size_t height)
 		return (left + 1);
 	return (right + 1);
 }
-
-/**
- *This line of code calculate the height of a binary tree recursively. It checks
- *if the left subtree has a greater height than the right subtree, and if so,
- *returns the height of the left subtree. Otherwise, it returns the height of
- *the right subtree. The ?: operator is a ternary operator that takes three
- *operands: a boolean expression, a value to return if the boolean expression
- *is true, and a value to return if the boolean expression is false. In this
- *case, the boolean expression is left_height > right_height, the value to
- *retrn if true is left_height,and the value to return if false is right_height.
- *The + 1 at the end of the line adds 1 to the height to account for the root
- *node of the binary tree
- */
