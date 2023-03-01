2e9f089c6dbdfab1c0677f788fd0bc9def113191
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-01 00:04:49 -0800

    insert right to a node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_insert_right - inserts a node as the right child of another node
+ *@parent: pointer to the node to insert the right child in
+ *@value: the value to store in the new dance
+ *Return: the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right_child;
+
+	if (parent == NULL)
+		return (NULL);
+
+	right_child = binary_tree_node(parent, value);
+
+	if (right_child == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		parent->right->parent = right_child;
+		right_child->right = parent->right;
+	}
+
+	parent->right = right_child;
+
+	return right_child;
+}
--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 /*BASIC BINARY TREE*/
 /**
  * struct binary_tree_s - Binary tree node
@@ -35,10 +36,10 @@ typedef struct binary_tree_s heap_t;
 /*prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 /*PRINT FUNCTION DISPLAY A TREEE*/
 void binary_tree_print(const binary_tree_t *);
-
 /*END OF TREE DISPLAY*/
 
 #endif
